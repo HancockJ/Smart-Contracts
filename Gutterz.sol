1ab857a2779516e44fcaf97fa75512e083438ef3
jackhancock <hancockjack99@gmail.com>
2022-07-30 15:34:48 -0400

    Added back basic URI functionality

--- Gutterz.sol
+++ Gutterz.sol
@@ -27,6 +27,21 @@
                         \KARMELEONS/
 
 */
+
+/*
+Rinkeby contracts
+    IERC1155 public CATS_ADDRESS = IERC1155(0x66C8f2Aa66e5745D62D4920Fc40d2662042Cc688);
+    IERC1155 public RATS_ADDRESS = IERC1155(0xEb2a81d99E8604FC08372b5Fe008F3EE338185E1);
+    IERC721Enumerable public PIGEONS_ADDRESS = IERC721Enumerable(0x47804DFcdF243DFcdb0be950DFFBB13386762a7E);
+    IERC721Enumerable public DOGS_ADDRESS = IERC721Enumerable(0xd1f54655a01E88b40BcD5925504648C418e00399);
+
+Mainnet contracts
+    IERC1155 public CATS_ADDRESS = IERC1155(0xEdB61f74B0d09B2558F1eeb79B247c1F363Ae452);
+    IERC1155 public RATS_ADDRESS = IERC1155(0xD7B397eDad16ca8111CA4A3B832d0a5E3ae2438C);
+    IERC721Enumerable public PIGEONS_ADDRESS = IERC721Enumerable(0x950b9476a4de757BB134483029AC4Ec17E739e3A);
+    IERC721Enumerable public DOGS_ADDRESS = IERC721Enumerable(0x6E9DA81ce622fB65ABf6a8d8040e460fF2543Add);
+*/
+
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -46,14 +61,20 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
 
     Counters.Counter private supply;
 
+    bool public paused = true;
+    bool public revealed = false;
+    string public NOT_REVEALED_URI = "NOT_REVEALED";
+
+    string public uriPrefix = "";
+    string public uriSuffix = ".json";
+
     uint256 public MAX_SUPPLY = 3000;
+
     IERC1155 public CATS_ADDRESS = IERC1155(0xEdB61f74B0d09B2558F1eeb79B247c1F363Ae452);
     IERC1155 public RATS_ADDRESS = IERC1155(0xD7B397eDad16ca8111CA4A3B832d0a5E3ae2438C);
     IERC721Enumerable public PIGEONS_ADDRESS = IERC721Enumerable(0x950b9476a4de757BB134483029AC4Ec17E739e3A);
     IERC721Enumerable public DOGS_ADDRESS = IERC721Enumerable(0x6E9DA81ce622fB65ABf6a8d8040e460fF2543Add);
 
-
-    mapping(address => uint) public CLAIM_COUNT;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
 
@@ -93,9 +114,9 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     /// @param _amount How many Gutterz to mint
     function mint(uint _amount, uint _id) public mintCompliance(_amount) nonReentrant {
         require(hasGutterID(msg.sender, _id), "You need to own a Gutter Animal to mint a Gutterz!");
-        require(3 - CLAIM_COUNT[msg.sender] - _amount >= 0, "You can only claim 3 Gutterz per wallet.");
+        require(3 - balanceOf(msg.sender) - _amount >= 0, "You can only claim 3 Gutterz per wallet.");
+        require(!paused, "The contract is paused!");
         for(uint i=0; i < _amount; i++) {
-            CLAIM_COUNT[msg.sender] += 1;
             supply.increment();
             _mint(msg.sender, supply.current());
         }
@@ -104,7 +125,6 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     /// @notice Allows the owner to mint more than the wallet limit
     function ownerMint(uint _amount, address _to) public onlyOwner mintCompliance(_amount) {
         for(uint i=0; i < _amount; i++) {
-            CLAIM_COUNT[msg.sender] += 1;
             supply.increment();
             _mint(_to, supply.current());
         }
@@ -117,6 +137,45 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
             gutterzOwners[i -1] = ownerOf(i);
         }
         return gutterzOwners;
+    }
+
+    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        if(!revealed) {
+            return NOT_REVEALED_URI;
+        }
+
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
+        : "";
+    }
+
+    function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+        uriPrefix = _uriPrefix;
+    }
+
+    function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+        uriSuffix = _uriSuffix;
+    }
+
+    /// @notice Allows owner to start and stop minting process
+    /// @param _state true = paused, false = not paused
+    function setPaused(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function withdraw() public onlyOwner {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return uriPrefix;
     }
 
 }
