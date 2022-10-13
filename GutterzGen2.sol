e8cd2589a1e90549e5b0f52f5795633e79caffb3
jackhancock <hancockjack99@gmail.com>
2022-10-13 08:22:03 -0400

    Gutterz Species 2 Contract is ready for it's final testing.

--- GutterzGen2.sol
+++ GutterzGen2.sol
@@ -1,24 +1,29 @@
 //SPDX-License-Identifier: MIT
 /*
-Gutterz Gen 2 Mint:
-
-Contract requirements:
-
-- 2,000 supply
-- .07 Public Mint
-
-Wallet must hold for a free mint:
-- 1 UNUSED Karmeleon
-- 1 Gutterz
-
-- Ability to change the price of the public mint
-- Look into hardcoding royalties into the contract
-
-UI updates:
-- Reuse Gutterz repo
-- Display .07 mint price/button if you hold only one Gutterz or one Karmeleon, or neither. Text should read something like "If you hold both a Gutterz and a Karmeleon, you can mint this NFT for free." and have links to mint a Karmeleon & buy a Gutterz
-- If user holds both, UI shows free mint with increment counter tied to how many they are eligible for based off of # of Karmeleons held
-- Include a karmz-like check to see if a Karmeleon is eligible to mint a Gen 2 free
+     ^\                                               /^
+    @@@@@                                           @@@@@
+   @@@@@@&                                        &@@@@@@
+   @@  @@@@@@                                   @@@@@@  @
+   #@   .@@@@@@@@     /@@@@@@@@@@@@@@@\     @@@@@@@@@.  @
+    @#  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&  #@
+     @&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&@
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+      @@@@@@@@@@@@@@@@   GUTTERZ 2   @@@@@@@@@@@@@@@@@@
+      @@@@@@@@@@@@@@ BY THE KARMELEONS @@@@@@@@@@@@@@@@
+      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+     @@@     @@@@@@@      /@@@@@@\      @@@@@@@     @@@
+      @@@@     @@@/     @@@@@@@@@@@@     \@@@     @@@@
+      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+      @@@@@@@@@@@@@@@@@@@@@@____@@@@@@@@@@@@@@@@@@@@@@@
+       @@@@@@@@@@@@@@@@@@@@\    /@@@@@@@@@@@@@@@@@@@@
+          @@@@@@@@@@@@@@@@@@@||@@@@@@@@@@@@@@@@@@@
+             @@@@@@@@@@@@@@@@/\@@@@@@@@@@@@@@@@
+               @@@@@/-------/@@\-------\@@@@@
+                \@@/@@@@@@@@@@@@@@@@@@@@\@@/
+                     \@@@@@@@@@@@@@@@@/
 */
 
 pragma solidity >=0.7.0 <0.9.0;
@@ -33,8 +38,8 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @title Gutterz Gen 2 free mint contract
 /// @author Jack (@DblJackDiamond) on Twitter
-/// @dev
-contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
+/// @dev All function calls are currently implemented without side effects
+contract GutterzTwo is ERC721, Ownable, ReentrancyGuard  {
     using Strings for uint256;
     using Counters for Counters.Counter;
 
@@ -45,7 +50,7 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     mapping(uint => bool) public CLAIMED;
 
     // Switch for turning public mint on and off
-    bool public PUBLIC_MINT_ON = false;
+    bool public PUBLIC_MINT_ON = true;
     // Price if going through public mint
     uint256 public PUBLIC_COST = 0.07 ether;
 
@@ -60,11 +65,11 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
 
     string public _name = "Gutterz Species 2";
     string public _symbol = "GTRZ2";
-    uint256 public MAX_SUPPLY = 1000;
+    uint256 public MAX_SUPPLY = 2000;
 
 
-    IERC721Enumerable public KARMELEONS_ADDRESS = IERC721Enumerable(0x950b9476a4de757BB134483029AC4Ec17E739e3A);
-    IERC721Enumerable public GUTTERZ_ADDRESS = IERC721(0xB71b0a17E21a0D1BF4f07858bCd6B18A985467e5);
+    IERC721Enumerable public KARMELEONS_CONTRACT = IERC721Enumerable(0xD551B203775c92279165fA69B5B8c1ee6ecEA793);
+    IERC721 public GUTTERZ_CONTRACT = IERC721(0x09c279dfADA0935B748b104A687Ae9C2531d7263);
 
 
     constructor() ERC721(_name, _symbol) {}
@@ -78,7 +83,7 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     /// @param _address Address of account to check for a Gutterz Species 1
     /// @return bool True if the address owns a Gutter animal with that ID
     function hasGutterz(address _address) public view returns (bool) {
-        if(GUTTERZ_ADDRESS.balanceOf(_address) > 0 ){
+        if(GUTTERZ_CONTRACT.balanceOf(_address) > 0 ){
             return true;
         }
         return false;
@@ -89,6 +94,14 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
         return supply.current();
     }
 
+    function checkWalletEligibility(address _wallet) public view returns (bool, uint) {
+        if(hasGutterz(_wallet)){
+            return (true, unusedKarmeleons(_wallet).length);
+        } else {
+            return (false, 0);
+        }
+    }
+
     ////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////// 3 Mint types - free, public, and owner //////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////
@@ -96,11 +109,12 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     /// @notice Checks to make sure msg.sender is eligible to mint the desired amount of Gutterz
     /// @param _amount How many Gutterz to mint
     function holdersMint(uint _amount) public mintCompliance(_amount) nonReentrant {
-        require(hasGutterID(msg.sender), "You need to own a Gutterz Species 1 to mint a Gutterz species 2 for free!");
+        require(hasGutterz(msg.sender), "You need to own a Gutterz Species 1 to mint a Gutterz species 2 for free!");
         uint[] memory freeMintsRemaining = unusedKarmeleons(msg.sender);
-        require(freeMintsRemaining.length >= _numberOfTokens, "You don't own enough unused Karmeleons!");
+        require(freeMintsRemaining.length >= _amount, "You don't own enough unused Karmeleons!");
         require(!paused, "The contract is paused!");
         for(uint i=0; i < _amount; i++) {
+            CLAIMED[freeMintsRemaining[i]] = true;
             supply.increment();
             _mint(msg.sender, supply.current());
         }
@@ -110,7 +124,7 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     /// @param _amount How many Gutterz to mint
     function publicMint(uint _amount) public payable mintCompliance(_amount) nonReentrant {
         require(PUBLIC_MINT_ON, "Minting only available to eligible holders right now");
-        require(msg.value >= PUBLIC_COST * _mintAmount, "Insufficient payment sent to mint");
+        require(msg.value >= PUBLIC_COST * _amount, "Insufficient payment sent to mint");
         require(!paused, "The contract is paused!");
         for(uint i=0; i < _amount; i++) {
             supply.increment();
@@ -143,19 +157,17 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     /// @param _owner Address of account to check for Karmeleons
     /// @return uint[] Array of Eligible karmeleons by ID.
     function unusedKarmeleons(address _owner) internal view returns (uint[] memory) {
-        uint ownedKarmeleonCount;
-        uint validKarmeleonCount;
-        uint ownedKarmeleonCount = IERC721Enumerable(KARMELEONS_ADDRESS).balanceOf(msg.sender);
+        uint ownedKarmeleonCount = KARMELEONS_CONTRACT.balanceOf(_owner);
         uint validKarmeleonCount = 0;
         for(uint i=0; i < ownedKarmeleonCount; i++){
-            if(!CLAIMED[IERC721Enumerable(KARMELEONS_ADDRESS).tokenOfOwnerByIndex(_owner, i)]){
+            if(!CLAIMED[KARMELEONS_CONTRACT.tokenOfOwnerByIndex(_owner, i)]){
                 //Karmeleon is valid for mint
                 validKarmeleonCount++;
             }
         }
         uint[] memory validKarmeleons = new uint[](validKarmeleonCount);
         for(uint i=0; i < ownedKarmeleonCount; i++){
-            uint karmeleonID = IERC721Enumerable(KARMELEONS_ADDRESS).tokenOfOwnerByIndex(_owner, i);
+            uint karmeleonID = KARMELEONS_CONTRACT.tokenOfOwnerByIndex(_owner, i);
             if(!CLAIMED[karmeleonID]){
                 validKarmeleons[validKarmeleonCount - 1] = karmeleonID;
                 validKarmeleonCount--;
@@ -196,6 +208,13 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
         revealed = true;
     }
 
+
+    /// @notice Allows owner to start and stop the public mint
+    /// @param _state true = paused, false = not paused
+    function setPublicMint(bool _state) public onlyOwner {
+        PUBLIC_MINT_ON = _state;
+    }
+
     /// @notice Allows owner to start and stop minting process
     /// @param _state true = paused, false = not paused
     function setPaused(bool _state) public onlyOwner {
@@ -210,7 +229,4 @@ contract Gutterz is ERC721, Ownable, ReentrancyGuard  {
     function _baseURI() internal view virtual override returns (string memory) {
         return uriPrefix;
     }
-
 }
-
-
